f1abe7675b1d5cfd36f469aa820c10fa47337674
Casper Beyer <caspervonb@qup.io>
2015-09-20 17:14:53 +0800

    lib: emit browser event when process is available
    Currently there is no clean way of getting access to the browser process
    created by the `browser` plugin.
    
    This emits a `browser` event on the `Runner` object when the browser
    process has been succesfully created in the `browser` plugin.

--- test/lib_browser.js
+++ test/lib_browser.js
@@ -13,7 +13,7 @@ commands.forEach(function (command, index) {
   var port = 4000 + index;
 
   test('open url in ' + command, function (test) {
-    test.plan(3);
+    test.plan(4);
 
     var runner = amok.createRunner();
     runner.on('close', function () {
@@ -23,6 +23,11 @@ commands.forEach(function (command, index) {
     runner.set('url', url.resolve('file://', path.join('/' + __dirname, '/fixture/basic/index.html')));
 
     runner.use(amok.browser(port, command));
+
+    runner.on('browser', function(browser) {
+      test.ok(browser);
+    });
+
     runner.connect(port, 'localhost', function () {
       runner.client.console.on('data', function (message) {
         test.equal(message.text, 'ready');
@@ -53,6 +58,7 @@ commands.forEach(function (command, index) {
 
     server.on('listening', function() {
       var runner = amok.createRunner();
+
       server.on('close', function () {
         runner.close();
       });
